d4152f3c087df7d382451fa8dd8be03d83b489fa
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-07-07 22:40:28 +0200

    add performances script

--- assignment1/src/Performance.java
+++ assignment1/src/Performance.java
@@ -8,10 +8,10 @@ public class Performance {
     public static void main(String[] argv) {
         Board model = new Board();
         model.init(new config.MassiveBoardConf());
-
         int iterations = 2000;
         int i = 0;
         long total = 0;
+        long avg = 0;
         long lastUpdateTime = System.currentTimeMillis();
         while (i++ < iterations) {
             long current = System.currentTimeMillis();
@@ -20,9 +20,12 @@ public class Performance {
             lastUpdateTime = current;
             model.updateState(elapsed);
             if (i % 100 == 0) {
+                avg += total;
                 System.out.println("Avg at pass " + i + " : " + ((double) total) / 100);
                 total = 0;
             }
         }
+        System.out.println("Avg total: " + (double)avg/iterations);
     }
+
 }
